4ea0b7122f2b9bc6597b704ea9b778b68a8c6180
kaushani <kaushanichowdhury52@gmail.com>
2023-05-18 08:15:15 +0530

    table Data

--- src/pages/AllToy/AllToy.jsx
+++ src/pages/AllToy/AllToy.jsx
@@ -6,27 +6,51 @@ const AllToy = () => {
   const loadedAllToy = useLoaderData();
   //   console.log(loadedAllToy);
   return (
-    <div className="md:px-12 p-4">
+    <div className="md:px-12 mt-8 p-4">
       <div>
-        <div className="overflow-x-auto">
+        <h2 className="text-center bg-[#FFD95A] p-3 underline font-['cursive'] text-4xl font-bold tracking-widest ">
+          All Toys Here
+        </h2>
+      </div>
+      <div>
+        <div className=" mt-8 overflow-x-auto">
           <table className="table w-full">
             {/* head */}
             <thead>
               <tr>
-                <th></th>
-                <th>Seller</th>
-                <th>Toy Name</th>
-                <th>Image</th>
-                <th>Sub-category</th>
-                <th>Price</th>
-                <th>Available Quantity</th>
-                <th></th>
+                <th className="bg-[#66347F] text-white font-serif font-bold"></th>
+                <th className="bg-[#66347F] text-white font-serif font-bold text-base">
+                  Seller
+                </th>
+                <th className="bg-[#66347F] text-white font-serif font-bold text-base">
+                  Toy Name
+                </th>
+                <th className="bg-[#66347F] text-white font-serif font-bold text-base">
+                  Image
+                </th>
+                <th className="bg-[#66347F] text-white font-serif font-bold text-base">
+                  Sub-category
+                </th>
+                <th className="bg-[#66347F] text-white font-serif font-bold text-base">
+                  Price
+                </th>
+                <th className="bg-[#66347F] text-white font-serif font-bold text-base">
+                  Available <br />
+                  Quantity
+                </th>
+                <th className="bg-[#66347F] text-white font-serif font-bold text-base">
+                  Details
+                </th>
               </tr>
             </thead>
             <tbody>
               {/* row 1 */}
-              {loadedAllToy.map((toys) => (
-                <AllToyData toys={toys} key={toys._id}></AllToyData>
+              {loadedAllToy.map((toys, index) => (
+                <AllToyData
+                  toys={toys}
+                  index={index + 1}
+                  key={toys._id}
+                ></AllToyData>
               ))}
             </tbody>
           </table>
--- src/pages/AllToyData/AllToyData.jsx
+++ src/pages/AllToyData/AllToyData.jsx
@@ -1,13 +1,39 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
-const AllToyData = ({ toys }) => {
-  console.log(toys);
+const AllToyData = ({ toys, index }) => {
+  //   console.log(toys);
+  console.log(index);
+  const {
+    _id,
+    sellername,
+    email,
+    name,
+    pic,
+    subcategory,
+    price,
+    rating,
+    quantity,
+    details,
+  } = toys;
   return (
-    <tr>
-      <th>1</th>
-      <td>Cy Ganderton</td>
-      <td>Quality Control Specialist</td>
-      <td>Blue</td>
+    <tr className=" font-serif">
+      <th>{index}</th>
+      <td>{sellername}</td>
+      <td>{name}</td>
+      <td>
+        <img src={pic} className="w-10 h-10 rounded-full" alt="" />
+      </td>
+      <td>{subcategory}</td>
+      <td>{price}k</td>
+      <td>{quantity}</td>
+      <td>
+        <Link>
+          <button className="bg-[#66347F] text-white font-serif px-2 py-1 rounded-lg">
+            View Details
+          </button>
+        </Link>
+      </td>
     </tr>
   );
 };
